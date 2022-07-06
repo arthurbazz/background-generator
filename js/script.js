d60bc638ac3ba1abe6b278ed68676a1e8574c463
Chibuzor Arthur Uzoaru <chibuzorarthur@gmail.com>
2022-07-06 14:10:05 +0100

    refactor: colors function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
 const btn = document.querySelector("button");
 
-// checking if the scropt is connected
-// console.log(css); //console.log("h3") just prints out the value in quotes.
-// console.log(color1);
-// console.log(color2);
-function colorGradient() {
+
+const colorGradient = () => {
     body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
     // css background rgb
     css.textContent = body.style.background;
     // btn color on the first page load
     btn.style.background = `linear-gradient(to left, ${color1.value}, ${color2.value})`;
 }
+
 // generating randon numbers
-function rnd() {
+const rnd = () => {
     return (Math.floor(Math.random() * 255));
 }
+
 // genrating randomColors
-function rndColors() {
+const rndColors = () => {
     const rndCol1 = `rgb(${rnd(255)}, ${rnd(255)}, ${rnd(255)})`;
     const rndCol2 = `rgb(${rnd(255)}, ${rnd(255)}, ${rnd(255)})`;
     // displaying randomColors 
@@ -32,7 +31,6 @@ function rndColors() {
     color2.style.background = rndCol2;
     // css background rgb
     css.textContent = body.style.background;
-    
 }
 
 color1.addEventListener("input", colorGradient); 
